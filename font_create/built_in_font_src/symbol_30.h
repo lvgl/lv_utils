3720a07986824c90c46cfdff09534c60f2c19f24
Gabor Kiss-Vamosi <kisvegabor@gmail.com>
2017-10-02 17:16:59 +0200

    add symbol fonts built_in fonts

---
+++ font_create/built_in_font_src/symbol_30.h
@@ -0,0 +1,17 @@
+#ifndef SYMBOL_30_H
+#define SYMBOL_30_H
+
+/*Use UTF-8 encoding in the IDE*/
+
+#include "misc_conf.h"
+
+#if  USE_FONT_SYMBOL_30 != 0
+
+#include <stdint.h>
+#include "../font.h"
+
+font_t * symbol_30_get_dsc(void);
+
+#endif   /*USE_FONT_SYMBOL_30 != 0*/
+
+#endif   /*SYMBOL_30_H*/
