17198ad41d8d191b1490e7569df5f31ae18bc336
Gabor Kiss-Vamosi <kisvegabor@gmail.com>
2017-12-08 09:13:06 +0100

    img_conv: put 8, 16 and 24 color depth arrays into .c file

--- img_conv/img_conv.py
+++ img_conv/img_conv.py
@@ -24,8 +24,8 @@ def conf_init(argv):
             print '   python img_conv.py -f <file name> [-c <color depth> -t <chroma key>]\n'
             print 'Options' 
             print '  -f, --file         name of image to convert(e.g. my_pic.png)'
-            print '  -c, --colordepth   color depth, 8/16/24.                                    Optional, default: 16 '
-            print '  -t, --chromakey    mark as chrmakeyed (no value)                            Optional, default: off'
+            print '  -c, --colordepth   color depth for the binary file, 8/16/24.                       Optional, default: 16 '
+            print '  -t, --chromakey    mark as chrmakeyed (no value) needed                            Optional, default: off'
             print 'Example' 
             print '  Convert an image with 24 bit color depth and no chroma key'              
             print '    python img_conv.py -f file1.png -c 24\n'
@@ -69,7 +69,7 @@ def img_proc():
     (w,h) = img.size 
     data = list(img.getdata())
     print "Size: ",  w, "x", h
-    print "Color depth: " + cd
+    print "Bin. color depth: " + cd
     print "Chroma keyed: " + transp
     print "-----------------"
 
@@ -79,9 +79,8 @@ def img_proc():
     fn_txt = "img_" + fn_base + ".c"
     f_txt = open(fn_txt, 'w')
 
-    #Write txt the header
+    #Write txt to the header
     inc = "#include <stdint.h> \r\n#include \"lvgl/lv_misc/lv_color.h\"\r\n\r\n"
-    inc += "#if LV_COLOR_DEPTH != " + cd + "\r\n#error \"img_" + fn_base + ": Color depth mismatch\"\r\n#else\r\n\r\n"
     f_txt.write(inc)
 
     #Create the output bin file
@@ -106,17 +105,27 @@ def img_proc():
     f_bin.write(pack('<I', header))
 
     #Write the c header
-    f_txt.write("const color_int_t img_" + fn_base + "[] = {\r\n")
-    dsc = "/*HEADER\r\n   Width = " + str(w) + "\r\n   Height = " + str(h) + "\r\n   Chroma keyed: " + transp + "\r\n   Color depth: " + cd + "*/\r\n"
-    if cd == "8":
-        dsc +=  str(header & 0xFF) + ", " + str((header >> 8) & 0xFF) + ", " + str((header >> 16) & 0xFF) + ", " + str((header >> 24) & 0xFF) + ","  
-    elif cd == "16":
-        dsc += str(header & 0xFFFF) + ", " + str((header >> 16) & 0xFFFF) + ","
-    elif cd == "24":
-        dsc += str(header) + ","
+    txt_c8 = ""
+    txt_c16 = ""
+    txt_c24 = ""
 
-    dsc += "\r\n\r\n/*IMAGE DATA*/ \r\n\r\n"
-    f_txt.write(dsc)
+    dsc = "\r\n"
+    dsc +=  "/* Width = " + str(w) + "\r\n"
+    dsc +=  " * Height = " + str(h) + "\r\n"  
+    dsc +=  " * Chroma keyed: " + transp + "\r\n"
+    dsc +=  " * Color depth: " + cd + "\r\n"
+    dsc +=  " */\r\n\r\n"
+
+    f_txt.write(dsc)   
+
+    f_txt.write("const lv_color_int_t img_" + fn_base + "[] = {\r\n")
+    
+    txt_c8 += "#if LV_COLOR_DEPTH == 8\r\n  " + str(header & 0xFF) + ", " + str((header >> 8) & 0xFF) + ", " + str((header >> 16) & 0xFF) + ", " + str((header >> 24) & 0xFF) + ","
+    txt_c8 += "  /* HEADER */\r\n\r\n  /*IMAGE DATA*/\r\n  "
+    txt_c16 += "#elif LV_COLOR_DEPTH == 16\r\n  " + str(header & 0xFFFF) + ", " + str((header >> 16) & 0xFFFF) + ","
+    txt_c16 += "  /* HEADER */\r\n\r\n  /*IMAGE DATA*/\r\n  "
+    txt_c24 += "#elif LV_COLOR_DEPTH == 24\r\n  " + str(header) + ","
+    txt_c24 += "  /* HEADER */\r\n\r\n  /*IMAGE DATA*/\r\n  "
 
     print "Converting... "
 
@@ -125,43 +134,56 @@ def img_proc():
     line = 0
     for px in data:
         try:
-            if cd == "8":
-                r =  px[0] >> 6
-                g =  px[1] >> 5
-                b =  px[2] >> 5
-                px_out = (r << 5) + (g << 2) + b
-                f_bin.write(pack('<B', px_out))
-            elif cd == "16":
-                r =  px[0] >> 3
-                g =  px[1] >> 2
-                b =  px[2] >> 3
-                px_out = (r << 11) + (g << 5) + b
-                f_bin.write(pack('<H', px_out))
-            elif cd == "24":
-                r =  px[0]
-                g =  px[1]
-                b =  px[2]
-                px_out = (r << 16) + (g << 8) + b
-                f_bin.write(pack('<L', px_out))
+            # 8 bit colors
+            r =  px[0] >> 6
+            g =  px[1] >> 5
+            b =  px[2] >> 5
+            px_out = (r << 5) + (g << 2) + b
+            txt_c8 += str(px_out) + ", "
+            if cd == "8": f_bin.write(pack('<B', px_out))
+
+            # 16 bit colors
+            r =  px[0] >> 3
+            g =  px[1] >> 2
+            b =  px[2] >> 3
+            px_out = (r << 11) + (g << 5) + b
+            txt_c16 += str(px_out) + ", "
+            if cd == "16": f_bin.write(pack('<H', px_out))
+            
+            # 24 bit colors
+            r =  px[0]
+            g =  px[1]
+            b =  px[2]
+            px_out = (r << 16) + (g << 8) + b
+            txt_c24 += str(px_out) + ", "
+            if cd == "24": f_bin.write(pack('<L', px_out))
+        
         except TypeError as te:
             print "ERROR:", te 
             print "ERROR: Convert the image to .jpg or .png format and try again!"
             exit()
 
-        f_txt.write(str(px_out))
-        f_txt.write(", ")
-
         col += 1
-        if col % w == 0:
+        if col % w == 0:    #Start a new line in c file
             line += 1
-            f_txt.write("\r\n")
+            txt_c8 += "\r\n  "
+            txt_c16 += "\r\n  "
+            txt_c24 += "\r\n  "
             sys.stdout.write("\r%d%%" % ((line * 100) / h)) #Wirte percentage
             sys.stdout.flush()
     
-    print ""		#New line after % count
+    print ""		#New line after percentage count
 
-    f_txt.write("};\r\n\r\n")
-    f_txt.write("#endif\t/*Color depth check*/\r\n")
+    txt_c8 += "\r\n\r\n"
+    txt_c16 += "\r\n\r\n"
+    txt_c24 += "\r\n#else\r\n"
+    
+
+    #Write the c array
+    f_txt.write( txt_c8 + txt_c16 + txt_c24)
+    f_txt.write('/*Color depth check*/\r\n#error  "Invalid color depth"\r\n#endif')
+    f_txt.write("\r\n};  /*Arrray end*/")
+
 
     #Close files
     f_txt.close()
